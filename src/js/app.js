f1e86c6f21eca4637b2ceafebc97a57df2098c7b
mingkj <yueruming@gmail.com>
2017-05-07 23:41:46 +0800

    marker api https

--- src/js/app.js
+++ src/js/app.js
@@ -215,7 +215,7 @@ var Place = function (data) {
     // 这个函数生成不同颜色的 marker 图标，参数分别为 url size origin anchor scaledSize
     var makeMarkerIcon = function (markerColor) {
         return new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+            'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
             '|40|_|%E2%80%A2',
             new google.maps.Size(22, 40),
             new google.maps.Point(0, 0),
